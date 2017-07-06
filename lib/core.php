c531525a66d7fae030e89c303c03b098cff020b0
Sarvaritdinov R <ra9oaj@gmail.com>
2017-07-06 17:20:56 +0700

    *Added TK_GTaskPage class

--- lib/core.php
+++ lib/core.php
@@ -11,74 +11,12 @@
  * @link https://github.com/tehnokom/alfo-tk-global-tasks-plugin
  */
 
+require_once(TKGT_ROOT . 'lib/taskpage.php');
 require_once(TKGT_ROOT . 'lib/settings.php');
 require_once(TKGT_ROOT . 'lib/task.php');
 require_once(TKGT_ROOT . 'lib/tasks.php');
 require_once(TKGT_ROOT . 'lib/ajax_functions.php');
 
-function tkgt_admin_menu()
-{
-    add_menu_page( _x('TK Global Tasks', 'Admin menu', 'tkgt'),
-        _x('Global Tasks', 'Admin menu', 'tkgt'),
-        'manage_options',
-        TKGT_ROOT . 'admin/tasks_list.php',
-        '',
-        '',
-        "16.2");
+global $post;
 
-    add_options_page(_x('Tasks Settings', 'Admin menu', 'tkgt'),
-        _x('TK Tasks', 'Admin menu', 'tkgt'),
-        'manage_options',
-        TKGT_ROOT . 'admin/settings.php',
-        '');
-}
-add_action('admin_menu', 'tkgt_admin_menu');
-
-function tkgt_registry_styles()
-{
-    if(has_action('admin_enqueue_scripts')) {
-        wp_register_style('tkgt-admin-settings-css', TKGT_URL . 'admin/css/settings.css');
-        wp_enqueue_style('tkgt-admin-settings-css');
-    } else {
-
-    }
-}
-add_action('admin_enqueue_scripts', 'tkgt_registry_styles');
-add_action('wp_enqueue_scripts', 'tkgt_registry_styles');
-
-function tkgt_create_page()
-{
-    global $wp_rewrite;
-    $slug = TK_GTask::taskSettings(true)->slug;
-    //file_put_contents(__FILE__ . '.log',get_post_type(). "\r\n\r\n", FILE_APPEND);
-    add_rewrite_tag('%tkgt_page%', '([^&]+)');
-    add_rewrite_rule('^projektoj/([^/]+)' . $slug,
-         'index.php?post_type=projektoj&name=$matches[1]&tkgt_page=' . $slug,
-        'top');
-    /*add_rewrite_rule('^([^/]+)/' . $slug . '$',
-        'index.php?name=$matches[0]&tkgt_page=' . $slug,
-        'top');*/
-
-    $wp_rewrite->flush_rules();
-}
-add_action('init', 'tkgt_create_page');
-
-function tkgt_template_redirect()
-{
-    global $wp, $wp_query, $post;
-    //file_put_contents(__FILE__ . '.log',serialize($wp->query_vars). "\r\n\r\n", FILE_APPEND);
-    if(!empty($wp->query_vars['tkgt_page']) && !empty($post)) {
-
-    }
-}
-add_action('template_redirect', 'tkgt_template_redirect');
-
-function tkgt_template_include($template_path)
-{
-    if(in_array(get_post_type(),TK_GTask::taskSettings()['enabled_for'])) {
-        $template_path = TKGT_ROOT . 'styles/default/page.php';
-    }
-
-    return $template_path;
-}
-add_filter('template_include', 'tkgt_template_include');
+$tkgt_page = new TK_GTaskPage();
--- lib/taskpage.php
+++ lib/taskpage.php
@@ -15,7 +15,103 @@ class TK_GTaskPage
 {
     public function __construct($post_id)
     {
+        add_action('init', array($this, 'init'));
+        add_action('admin_menu', array($this, 'addAdminMenu'));
+        add_action('admin_enqueue_scripts', array($this, 'regJS_CSS'));
+        add_action('wp_enqueue_scripts', array($this, 'regJS_CSS'));
+        add_action('template_redirect', array($this, 'checkPage'));
+    }
 
+    public function init()
+    {
+        $this->regRewriteRules();
+    }
+
+    public function addAdminMenu()
+    {
+        add_menu_page(_x('TK Global Tasks', 'Admin menu', 'tkgt'),
+            _x('Global Tasks', 'Admin menu', 'tkgt'),
+            'manage_options',
+            TKGT_ROOT . 'admin/tasks_list.php',
+            '',
+            '',
+            "16.2");
+
+        add_options_page(_x('Tasks Settings', 'Admin menu', 'tkgt'),
+            _x('TK Tasks', 'Admin menu', 'tkgt'),
+            'manage_options',
+            TKGT_ROOT . 'admin/settings.php',
+            '');
+    }
+
+    public function regJS_CSS()
+    {
+        $this->regScripts();
+        $this->regStyles();
+    }
+
+    protected function regRewriteRules()
+    {
+        global $wp_rewrite;
+        $slug = TK_GTask::taskSettings(true)->slug;
+
+        add_rewrite_tag('%tkgt_page%', '([^&]+)');
+
+        //WP Posts
+        add_rewrite_rule('^([^/]+)/([^/]+)' . $slug . '$',
+            'index.php?post_type=$matches[1]&name=$matches[2]&tkgt_page=' . $slug,
+            'top');
+
+        //WP Static pages
+        add_rewrite_rule('^([^/]+)' . $slug . '$',
+            'index.php?pagename=$matches[1]&tkgt_page=' . $slug,
+            'top');
+
+        $wp_rewrite->flush_rules();
+    }
+
+    public function checkPage() {
+        global $wp, $wp_query, $post;
+
+        if (!empty($wp->query_vars['tkgt_page'])) {
+            if (!empty($post) && in_array($post->post_type, TK_GTask::taskSettings(true)->enabled_for)) {
+                add_filter('template_include', array($this, 'includeTemplate'));
+            } else {
+                $wp_query->set_404();
+                status_header(404);
+                get_template_part(404);
+                exit;
+            }
+        }
+    }
+
+    public function includeTemplate($template_path)
+    {
+        if (in_array(get_post_type(), TK_GTask::taskSettings()['enabled_for'])) {
+
+            $template_path = TKGT_ROOT . 'styles/default/page.php';
+        }
+
+        return $template_path;
+    }
+
+    protected function regStyles()
+    {
+        if (has_action('admin_enqueue_scripts')) {
+            wp_register_style('tkgt-admin-settings-css', TKGT_URL . 'admin/css/settings.css');
+            wp_enqueue_style('tkgt-admin-settings-css');
+        } else {
+
+        }
+    }
+
+    protected function regScripts()
+    {
+        if (has_action('admin_enqueue_scripts')) {
+
+        } else {
+
+        }
     }
 
 }
